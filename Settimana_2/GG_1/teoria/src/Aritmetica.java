2484b19db1413b5879ccf4ee53c728727f32288a
vitod-ag <116908757+vitod-ag@users.noreply.github.com>
2024-04-22 09:56:33 +0200

    commit teoria UNIT4 W2 D1

---
+++ Settimana_2/GG_1/teoria/src/Aritmetica.java
@@ -0,0 +1,5 @@
+public class Aritmetica {
+    public int divisione(int a, int b) {
+        return a/b;
+    }
+}
---
+++ Settimana_2/GG_1/teoria/src/Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\n**Teoria UNIT4 W2 D1**");
+    Aritmetica aritmetica = new Aritmetica();
+        System.out.println(aritmetica.divisione(9,3));
+
+    }
+}
